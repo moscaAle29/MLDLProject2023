97204bf23312732c487ef05f7ac30031ee0c9216
Admin <trietngo712000@gmail.com>
2023-05-12 18:27:13 +0200

    fix bug

--- client.py
+++ client.py
@@ -131,9 +131,9 @@ class Client:
                     print(f'{self.name}-{i}')
                     _, prediction = outputs.max(dim=1)
 
-                    images = Image.fromarray(images.cpu().unsqueeze(dim = 1).numpy())
-                    prediction = Image.fromarray(prediction.cpu().unsqueeze(dim = 1).numpy())
-                    labels = Image.fromarray(labels.cpu().unsqueeze(dim = 1).numpy())
+                    images = Image.fromarray(images.cpu().squeeze().numpy())
+                    prediction = Image.fromarray(prediction.cpu().squeeze().numpy())
+                    labels = Image.fromarray(labels.cpu().squeeze().numpy())
 
                     data.append([i, wandb.Image(images), wandb.Image(prediction),  wandb.Image(labels)])
         
