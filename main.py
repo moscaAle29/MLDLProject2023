43054a1de2883b78a59fc4eccfc8eca25b840749
Admin <trietngo712000@gmail.com>
2023-06-28 18:10:07 +0200

    add max epoch

--- main.py
+++ main.py
@@ -568,7 +568,7 @@ def create_vae_based_clusters(args):
     #pretrain
     print('pretrain on gta5')
     data_loader = DataLoader(pretrained_dataset, batch_size=4, shuffle=True, drop_last=True)
-    trainer = pl.Trainer(gpus=1)
+    trainer = pl.Trainer(gpus=1, max_epochs = 100)
     trainer.fit(net, train_dataloaders=data_loader)
 
     #fine_tuning
