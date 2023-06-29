f2c146bfddf088fb819a67b0ae34207e8df858d7
Alessandro <mosca.alessandro29@gmail.com>
2023-06-29 14:50:45 +0200

    small fixes

--- task2_data_collection.sh
+++ task2_data_collection.sh
@@ -3,6 +3,8 @@
 for clients_per_round in 2 4 8
 do
     for num_epochs in 1 3 6 9 12
-        python main.py --setting federated --dataset idda --dataset2 idda --model deeplabv3_mobilenetv2 --num_rounds 100 --num_epochs $num_epochs --clients_per_round $clients_per_round --rrc_transform --jitter
+    do
+        echo "running the file with " $clients_per_round "and " $num_epochs
+        python main.py --task_2_data_collection --setting federated --dataset idda --dataset2 idda --model deeplabv3_mobilenetv2 --num_rounds 100 --num_epochs $num_epochs --clients_per_round $clients_per_round --rrc_transform --jitter
     done
 done
