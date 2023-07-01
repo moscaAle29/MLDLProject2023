f6dc2accaedec94e890faf992700d270e5111d6b
Admin <trietngo712000@gmail.com>
2023-07-01 23:28:47 +0200

    change img transform for vae

--- main.py
+++ main.py
@@ -710,7 +710,7 @@ def get_dataset_vae():
     train_datasets = []
 
     #transform
-    resize = sstr.RandomResizedCrop(size=(96,96))
+    resize = sstr.Resize(size=(96,96)))
     normalization = sstr.Normalize(mean=[0.485, 0.456, 0.406],
                            std=[0.229, 0.224, 0.225])
     
